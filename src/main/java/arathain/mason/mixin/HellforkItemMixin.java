2ecdb1aeae6208e38861eabc059d333d9360ec55
cph101 <cph101@icloud.com>
2024-04-14 09:39:30 +0300

    Revert Impaled mixin refactor (crash in stable env)
    Signed-off-by: cph101 <cph101@icloud.com>

--- src/main/java/arathain/mason/mixin/HellforkItemMixin.java
+++ src/main/java/arathain/mason/mixin/HellforkItemMixin.java
@@ -2,7 +2,6 @@ package arathain.mason.mixin;
 
 import arathain.mason.init.MasonObjects;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -14,43 +13,39 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(HellforkItem.class)
 public class HellforkItemMixin {
-
-    // The detection methods
-    @ModifyReturnValue(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;", ordinal = 0))
-    private Block swapWithSoulLight1(BlockState instance, Block original) {
+    @Redirect(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;", ordinal = 0))
+    private Block swapWithSoulLight1(BlockState instance) {
         Block block = instance.getBlock();
-        if (block == MasonObjects.SOULLIGHT) return original;
+        if (block == MasonObjects.SOULLIGHT) return Blocks.SOUL_CAMPFIRE;
         return block;
     }
 
-    @ModifyReturnValue(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;", ordinal = 4))
-    private Block swapWithSoulLight2(BlockState instance, Block original) {
+    @Redirect(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;", ordinal = 7))
+    private Block swapWithSoulLight2(BlockState instance) {
         Block block = instance.getBlock();
-        if (block == MasonObjects.SOULLIGHT) return original;
+        if (block == MasonObjects.SOULLIGHT) return Blocks.SOUL_WALL_TORCH;
         return block;
     }
-
-    @ModifyReturnValue(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;", ordinal = 8))
-    private Block swapWithTorchLight1(BlockState instance, Block original) {
-        Block block = instance.getBlock();
-        if (block == MasonObjects.TORCHLIGHT) return original;
-        return block;
-    }
-
-    @ModifyReturnValue(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;", ordinal = 12))
-    private Block swapWithTorchLight2(BlockState instance, Block original) {
-        Block block = instance.getBlock();
-        if (block == MasonObjects.TORCHLIGHT) return original;
-        return block;
-    }
-
-    // The fork methods
 
     @ModifyExpressionValue(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getDefaultState()Lnet/minecraft/block/BlockState;", ordinal = 3))
     private BlockState swapWithSoulfork(BlockState original, ItemUsageContext ctx) {
         if (ctx.getWorld().getBlockState(ctx.getBlockPos()).getBlock() == MasonObjects.SOULLIGHT) {
             return MasonObjects.TORCHLIGHT.getDefaultState();
         } else return original;
+    }
+
+    @Redirect(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;", ordinal = 8))
+    private Block swapWithTorchLight1(BlockState instance) {
+        Block block = instance.getBlock();
+        if (block == MasonObjects.TORCHLIGHT) return Blocks.CAMPFIRE;
+        return block;
+    }
+
+    @Redirect(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;", ordinal = 15))
+    private Block swapWithTorchLight2(BlockState instance) {
+        Block block = instance.getBlock();
+        if (block == MasonObjects.TORCHLIGHT) return Blocks.WALL_TORCH;
+        return block;
     }
 
     @ModifyExpressionValue(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getDefaultState()Lnet/minecraft/block/BlockState;", ordinal = 7))
